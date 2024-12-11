640ae9c3e1a24cc27f44745507a55018e25e34f8
horiyee <kaito@horiyee.org>
2024-12-11 11:34:13 +0900

    chore: delete backend s3

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-up-and-running-state-hands-on"
-    key = "stage/data-stores/mysql/terraform.tfstate"
-    region = "us-east-1"
-
-    dynamodb_table = "terraform-up-and-running-locks-hands-on"
-    encrypt = true
-  }
-}
 provider "aws" {
   region = "us-east-1"
 }
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-up-and-running-state-hands-on"
-    key = "stage/services/webserver-cluster/terraform.tfstate"
-    region = "us-east-1"
-    
-    dynamodb_table = "terraform-up-and-running-locks-hands-on"
-    encrypt = true
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
