becb3ffe0c0f19cdd4fc2176031709a34c18336d
horiyee <kaito@horiyee.org>
2025-01-08 11:15:07 +0900

    Revert "chore: delete backend s3"
    This reverts commit ec07fe7f9c09b7e194e123a07781fdca1f658c2f.

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,3 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-hands-on"
+    key = "stage/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-1"
+    
+    dynamodb_table = "terraform-up-and-running-locks-hands-on"
+    encrypt = true
+  }
+}
+
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-up-and-running-state-hands-on"
+    key = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
