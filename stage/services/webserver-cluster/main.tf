a0aeee3fa2247187b4e405b6934b6cd752687fc5
horiyee <kaito@horiyee.org>
2025-01-08 11:37:30 +0900

    chore: delete aws_instance example

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -85,24 +85,6 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-01e3c4a339a264cc9"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-  
-  user_data_replace_on_change = true
-  
-  tags = {
-    Name = "terraform-example"
-  }
-}
-
 resource "aws_lb" "example" {
   name = "terraform-asg-example"
   load_balancer_type = "application"
